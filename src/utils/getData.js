9efcae5bd1be60e35d1d63943015656252268195
Leonardo Puebla <leonardo38361@gmail.com>
2022-03-18 00:41:31 -0300

    Operación math para obtener personajes aleatorios

--- src/utils/getData.js
+++ src/utils/getData.js
@@ -2,10 +2,11 @@ const API = process.env.API;
 
 const getData = async (id) => {
   const apiURl = id ? `${API}${id}` : API;
+  let randomCharacter = Math.floor(Math.random()*9);
   try {
     const response = await fetch(apiURl);
     const data = await response.json();
-    return data.results[0];
+    return data.results[randomCharacter];
   } catch (error) {
     console.log('Fetch Error', error);
   };
